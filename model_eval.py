773a4e07511aa1fdbd1d0fc332ddf020f445aec7
Angelawork <Qingchen.hu@mail.mcgill.ca>
2024-12-18 01:38:57 -0500

    repo management

--- model_eval.py
+++ model_eval.py
@@ -17,13 +17,13 @@ def get_results(data, preds):
 
 def test_prompt_template(example):
     return f"""### Instruction:
-        Fix the OCR errors in the provided text.
+Reconstruct the likely original text based on the OCR output provided.
 
-        ### Input:
-        {example}
+### Input:
+{example}
 
-        ### Response:
-        """
+### Response:
+"""
 
 weight_dir = "./model/bart-ocr-full"
 test = pd.read_csv("./dataset/test.csv")
@@ -56,5 +56,20 @@ for i in tqdm(range(len(test))):
     pred = tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][len(prompt):].strip()
     preds.append(pred)
 
+#batched processing
+# for s_i in tqdm(range(0, len(prompts), batch_size)):
+#     batch_p = prompts[s_i:s_i + batch_size]
+#     inputs = tokenizer(batch_p, max_length=1024, return_tensors="pt", truncation=True, padding=True).to(device)
+#     with torch.no_grad():
+#         outputs = model.generate(
+#             input_ids=inputs["input_ids"], 
+#             attention_mask=inputs["attention_mask"],
+#             max_new_tokens=1024
+#         )
+#     batch_preds = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+#     batch_preds = [
+#         pred[len(prompt):].strip() for prompt, pred in zip(batch_p, batch_preds)
+#     ]
+#     preds.extend(batch_preds)
 results = get_results(test, preds)
 results.to_csv("./bart-full.csv", index=False)
